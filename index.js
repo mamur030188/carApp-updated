875804ee480fc6a45528e22a41ed1ac076f47046
Mamirjon Mamadaliev <mike030188@gmail.com>
2021-08-16 15:14:50 +0900

    feat: ApolloServer depends on Graphql

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-const { graphql, buildSchema } = require('graphql')
-const express = require('express')
-const { graphqlHTTP } = require('express-graphql')
+const { ApolloServer, gql } = require('apollo-server')
 const db = require('./db.json')
 
 
 
-// create the schema
-const schema = buildSchema(`
+// create the schema . buildSchema replaces with "gql"
+const schema = gql(`
     enum CarTypes {
         Sedan
         SUV
@@ -28,35 +26,55 @@ const schema = buildSchema(`
     }
 `)
 
-// create the resolvers
-const resolvers = () => {
-    const carsByType = args => {
-        return db.cars.filter(car => car.type === args.type)
+// create the resolver map 
+const resolvers = {
+    Query: {
+        carsByType: (parent, args, context, info) => {
+            return db.cars.filter(car => car.type === args.type)
+        },
+        carsById: (parent, args, context, info) => {
+            return db.cars.filter(car => car.id === args.id) [0]
+        }
+    },
+    Car: {
+        brand: (parent, args, context, info) => {
+            return db.cars.filter(car => car.brand === parent.brand) [0].brand
+        }
+    },
+    Mutation: {
+        insertCar: (_, { brand, color, doors, type }) => {
+            db.cars.push({
+                id: Math.random().toString(),
+                brand: brand,
+                color: color,
+                doors: doors,
+                type: type
+            })
+            return db.cars
+        }
     }
-    const carsById = args => {
-        return db.cars.filter(car => car.id === args.id) [0]
-    }
-    const insertCar = ({ brand, color, doors, type }) => {
-        db.cars.push({
-            id: Math.random().toString(),
-            brand: brand,
-            color: color,
-            doors: doors,
-            type: type
-        })
-        return db.cars
-    }
-    return { carsByType, carsById, insertCar }
 }
 
-const app = express()
+const server = new ApolloServer({
+    typeDefs: schema,
+    resolvers
+})
 
-app.use('/graphql', graphqlHTTP({
-    schema: schema,
-    rootValue: resolvers(),
-    graphiql: true
-}))
+server.listen().then(({ url }) => {
+    console.log(`Server ready at ${url}`)
+})
 
-app.listen(3000)
 
-console.log('GraphQL server is listening on PORT 3000')
+
+
+// const app = express()
+
+// app.use('/graphql', graphqlHTTP({
+//     schema: schema,
+//     rootValue: resolvers(),
+//     graphiql: true
+// }))
+
+// app.listen(3000)
+
+// console.log('GraphQL server is listening on PORT 3000')
